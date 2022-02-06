0ceed35d962b7fd38d585fe60e7d0b363916ce34
Julian B <Keigun-Spark@outlook.com>
2022-02-06 03:50:56 +0100

    Update run_hwid_check.bat
    Added comments.
    Added functionality which tries to catch if no device, the wrong device or too many devices are plugged in.

--- run_hwid_check.bat
+++ run_hwid_check.bat
@@ -16,13 +16,17 @@ goto :stop
 @echo.
 @echo Verifying device data, please wait...
 
+set version_dvc='lighthouse_console.exe exit'
 set version_csl='lighthouse_console.exe version'
 set version_cmd='lighthouse_watchman_update.exe -aw3'
+
+set /a active_devices=0
 
 set serial_number=unknown
 
 set /a fw_version_current=0
-set /a fw_version_required=1632556731  
+set /a fw_version_required=1632556731
+:: Firmware version from file in \SteamVR\drivers\
 
 set hwid_current=0
 set product_id_check=f0000109
@@ -30,21 +34,37 @@ set product_id_required=f1030009
 
 set /a bl_version_current=0
 set /a bl_version_check=1622649182
-set /a bl_version_required=1629157907  
+set /a bl_version_required=1629157907
+:: Newer bootloader version of 1637337510 available?
+
+for /F "Tokens=1,5 delims= " %%A in (%version_dvc%) do (
+  if "%%A" EQU "Attached" (set /a active_devices=%%B)
+)
+
+@echo.
+if %active_devices% EQU 0 (@echo Could not find any active devices!)
+if %active_devices% EQU 0 goto :err
+if %active_devices% LSS 3 (@echo Wrong device connected?)
+if %active_devices% LSS 3 goto :err
+if %active_devices% GTR 3 (@echo Too many devices connected?)
+if %active_devices% GTR 3 goto :err
+:: Tries to catch if no device, the wrong device or too many devices are plugged in.
 
 for /F "Tokens=1,3 delims= " %%A in (%version_csl%) do (
   if "%%A" EQU "VRC" (set /a fw_version_current=%%B)
 )
-timeout /t 5 /nobreak >nul
+:: Check for firmware version.
 
 lighthouse_watchman_update.exe -bw3 >nul 2>&1
 timeout /t 8 /nobreak >nul
+:: Timeout of 8 or greater, mitigates issues with slow USB initialization.
 
 for /F "Tokens=1,3 delims= " %%A in (%version_cmd%) do (
   if "%%A" EQU "Hardware" (set hwid_current=%%B)
   if "%%A" EQU "Serial" (set serial_number=lhr-%%B)
   if "%%A" EQU "Bootloader" (set /a bl_version_current=%%B)
 )
+:: Check for HWID, serial and bootloader version.
 
 @echo.
 @echo -- Device status --
@@ -54,33 +74,47 @@ for /F "Tokens=1,3 delims= " %%A in (%version_cmd%) do (
 
 @echo.
 @echo Device HWID is 0x%hwid_current%
+if %hwid_current% EQU 0 (@echo HWID could not be identified, quit for safety!)
+if %hwid_current% EQU 0 goto :stop
 if %hwid_current% EQU %product_id_required% (@echo This is the correct HWID for Tundra Tracker.)
 if %hwid_current% EQU %product_id_required% goto :hwid_is_safe
 if %hwid_current% EQU %product_id_check% (@echo This is an older HWID and should be updated.)
 if %hwid_current% EQU %product_id_check% goto :bad_hwid
 @echo.
 @echo HWID of 0x%hwid_current% is not associated with Tundra Tracker, quit for safety!
+:: This catches all other possibilities.
 goto :stop
 
 :hwid_is_safe
 @echo.
 @echo Device firmware version is v%fw_version_current%
-if %fw_version_current% EQU %fw_version_required% (@echo This is the correct firmware version for Tundra Tracker.)
+if %fw_version_current% EQU 0 (@echo Firmware version could not be identified, quit for safety!)
+if %fw_version_current% EQU 0 goto :stop
+if %fw_version_current% EQU %fw_version_required% (@echo This is the correct firmware version for Tundra Tracker.) 
+:: Change 'EQU' to 'GEQ' if a newer version is not a issue and remove line further below.
 if %fw_version_current% LSS %fw_version_required% (@echo This is a older firmware version for Tundra Tracker.)
 if %fw_version_current% LSS %fw_version_required% goto :bad_fw_version
-if %fw_version_current% GTR %fw_version_required% (@echo This is a newer than expected firmware version for Tundra Tracker, quit for safety!)
+if %fw_version_current% GTR %fw_version_required% (@echo This is a newer than expected firmware version for Tundra Tracker, quit for safety!) 
+:: Remove ^ line if this is not a problem, added to catch unexpected behaviour.
 if %fw_version_current% GTR %fw_version_required% goto :stop
+:: Remove ^ line if this is not a problem, added to catch unexpected behaviour.
 
 @echo.
 @echo Device bootloader version is v%bl_version_current%
+if %bl_version_current% EQU 0 (@echo Bootloader version could not be identified, quit for safety!)
+if %bl_version_current% EQU 0 goto :stop
 if %bl_version_current% EQU %bl_version_required% (@echo This is the correct bootloader version for Tundra Tracker.)
 if %bl_version_current% EQU %bl_version_required% goto :stop
-if %bl_version_current% EQU %bl_version_check% (@echo This is a older bootloader version for Tundra Tracker.)
+if %bl_version_current% EQU %bl_version_check% (@echo This is a older bootloader version for Tundra Tracker.) 
+:: Change 'EQU' to 'LSS' to update all older bootloaders and remove line further below.
 if %bl_version_current% EQU %bl_version_check% goto :bad_bl_version
+:: Change 'EQU' to 'LSS' to update all older bootloaders and remove line further below.
 if %bl_version_current% GTR %bl_version_required% (@echo This is a newer than expected bootloader version for Tundra Tracker, quit for safety!)
 if %bl_version_current% GTR %bl_version_required% goto :stop
-if %bl_version_current% LSS %bl_version_required% (@echo This is a unexpected bootloader version for Tundra Tracker, quit for safety!)
+if %bl_version_current% LSS %bl_version_required% (@echo This is a unexpected bootloader version for Tundra Tracker, quit for safety!) 
+:: Remove ^ line when updating all older bootloaders.
 if %bl_version_current% LSS %bl_version_required% goto :stop
+:: Remove ^ line when updating all older bootloaders.
 goto :stop
 
 :err_hw_check
@@ -113,6 +147,7 @@ lighthouse_watchman_update.exe -Bw3 0xF1030009
 timeout /t 8 /nobreak >nul
 lighthouse_watchman_update.exe -bw3 >nul 2>&1
 timeout /t 8 /nobreak >nul
+:: Timeout of 8 or greater, mitigates issues with slow USB initialization.
 goto :check_compatibility
 
 :bad_fw_version
@@ -129,11 +164,13 @@ goto :bad_fw_version
 @echo.
 @echo -- Updating firmware --
 lighthouse_watchman_update.exe -Rw3 >nul 2>&1
-timeout /t 5 /nobreak >nul
+timeout /t 8 /nobreak >nul
 lighthouse_watchman_update.exe -s %serial_number% --target=application "C:\Program Files (x86)\Steam\steamapps\common\SteamVR\drivers\tundra_labs\resources\firmware\tracker\tundra-tracker_application_v1632556731.fw"
-timeout /t 5 /nobreak >nul
+:: Local firmware version distributed by SteamVR.
+timeout /t 8 /nobreak >nul
 lighthouse_watchman_update.exe -bw3 >nul 2>&1
 timeout /t 8 /nobreak >nul
+:: Timeout of 8 or greater, mitigates issues with slow USB initialization.
 goto :check_compatibility
 
 :bad_bl_version
@@ -149,15 +186,24 @@ goto :bad_bl_version
 :update_bl
 @echo -- Updating bootloader --
 lighthouse_watchman_update.exe -Rw3 >nul 2>&1
-timeout /t 5 /nobreak >nul
+timeout /t 8 /nobreak >nul
 lighthouse_watchman_update.exe -s %serial_number% --hwid 0x%hwid_current% --target=bootloader watchman_v3_bootloader_umodule.fw
-timeout /t 5 /nobreak >nul
+timeout /t 8 /nobreak >nul
 goto :check_compatibility
+
+:err
+@echo.
+@echo Please check connected devices!
+set /P c=Press Enter to quit...
+Exit /b
 
 :stop
 @echo.
 @echo Resetting device, please wait...
+:: To make sure the device is fully initialised before removal.
 lighthouse_watchman_update.exe -Rw3 >nul 2>&1
-timeout /t 5 /nobreak >nul
+timeout /t 8 /nobreak >nul
+@echo.
 @echo Device is now safe to unplug!
 set /P c=Press Enter to quit...
+Exit /b
